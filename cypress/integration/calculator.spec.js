dbbb93e6382581af63f4dad134dc6cc007fc578c
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-05-16 16:11:33 +0100

    Add integration test for display numbers

--- cypress/integration/calculator.spec.js
+++ cypress/integration/calculator.spec.js
@@ -4,7 +4,17 @@ describe("Calculator", () => {
   })
 
   it('should have working number buttons', () => {
+    cy.get('#number1').click();
+    cy.get('#number0').click();
     cy.get('#number2').click();
-    cy.get('.display').should('contain', '2')
+    cy.get('#number3').click();
+    cy.get('#number4').click();
+    cy.get('#number5').click();
+    cy.get('#number6').click();
+    cy.get('#number7').click();
+    cy.get('#number8').click();
+    cy.get('#number9').click();
+    cy.get('.display').should('have.text', '1023456789')
   })
+
 })
